bf0a4dcbc1afb48be1c958a296f2537ff1b61da7
othmanA <abah4506@stthomas.edu>
2013-09-24 13:22:19 -0400

    Fixes the coding style

--- SumReducer.java
+++ SumReducer.java
@@ -4,25 +4,12 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-/* 
- * To define a reduce function for your MapReduce job, subclass 
- * the Reducer class and override the reduce method.
- * The class definition requires four parameters: 
- *   The data type of the input key (which is the output key type 
- *   from the mapper)
- *   The data type of the input value (which is the output value 
- *   type from the mapper)
- *   The data type of the output key
- *   The data type of the output value
- */   
-public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-  /*
-   * The reduce method runs once for each key received from
-   * the shuffle and sort phase of the MapReduce framework.
-   * The method receives a key of type Text, a set of values of type
-   * IntWritable, and a Context object.
-   */
+public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	
+	// define a global intWritable instead of using new many times.
+	IntWritable intWritable = new IntWritable(); 
+
   @Override
 	public void reduce(Text key, Iterable<IntWritable> values, Context context)
 			throws IOException, InterruptedException {
@@ -32,17 +19,16 @@ public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 		 * For each value in the set of values passed to us by the mapper:
 		 */
 		for (IntWritable value : values) {
-		  
-		  /*
-		   * Add the value to the word count counter for this key.
-		   */
 			wordCount += value.get();
 		}
+		
+		// Set the intWritable value.
+		intWritable.set(wordCount);
 		
 		/*
 		 * Call the write method on the Context object to emit a key
 		 * and a value from the reduce method. 
 		 */
-		context.write(key, new IntWritable(wordCount));
+		context.write(key, intWritable);
 	}
 }
--- WordMapper.java
+++ WordMapper.java
@@ -5,25 +5,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-/* 
- * To define a map function for your MapReduce job, subclass 
- * the Mapper class and override the map method.
- * The class definition requires four parameters: 
- *   The data type of the input key
- *   The data type of the input value
- *   The data type of the output key (which is the input key type 
- *   for the reducer)
- *   The data type of the output value (which is the input value 
- *   type for the reducer)
- */
-
 public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  /*
-   * The map method runs once for each line of text in the input file.
-   * The method receives a key of type LongWritable, a value of type
-   * Text, and a Context object.
-   */
+	
+	IntWritable intWritable = new IntWritable();
+	Text text = new Text();
+	
+	
   @Override
   public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
@@ -33,22 +21,17 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
      * to a String object.
      */
     String line = value.toString();
-
-    /*
-     * The line.split("\\W+") call uses regular expressions to split the
-     * line up by non-word characters.
-     * 
-     * If you are not familiar with the use of regular expressions in
-     * Java code, search the web for "Java Regex Tutorial." 
-     */
+    
+    
     for (String word : line.split("\\W+")) {
       if (word.length() > 0) {
         
+    	  text.set(word);
         /*
          * Call the write method on the Context object to emit a key
          * and a value from the map method.
          */
-        context.write(new Text(word), new IntWritable(1));
+        context.write(text, intWritable);
       }
     }
   }
